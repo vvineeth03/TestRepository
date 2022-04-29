3a6a545556c6b4cbdcc4b47da5f9c7e44dcd141d
Vineeth Vasudevan <vineeth.vasudevan@gainwelltechnologies.com>
2022-04-29 14:23:32 -0600

    New Update to Print Numbers 1-10

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ConsoleApp
 {
@@ -6,7 +7,19 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Print ny=umbers 1 to 10 ");
+            List<int> numbers = new List<int>();
+            numbers.Add(1);
+            numbers.Add(2);
+            numbers.Add(3);
+            numbers.Add(4);
+            numbers.Add(5);
+            numbers.Add(6);
+            numbers.Add(7);
+            numbers.Add(8);
+            numbers.Add(9);
+            numbers.Add(10);
+            numbers.ForEach(el => Console.WriteLine(el));
         }
     }
 }
